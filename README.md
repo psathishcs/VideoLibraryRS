d3cc037d1c0bc9c4913800bdfa4a416ddac55ccd
DESKTOP-7D1NSHG\Sathish <p.sathish.cs@gmail.com>
2017-01-16 22:02:51 -0600

    Adding the project to Eureka Service Discovery Service.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # VideoLibraryRS
 # Swagger2 UI 
-   http://windows10.server.com:8085/vidoes/api/swagger-ui.html
+   http://windows10.server.com:8083/vidoes/api/swagger-ui.html
--- src/main/java/org/video/library/VideoLibraryApplication.java
+++ src/main/java/org/video/library/VideoLibraryApplication.java
@@ -2,6 +2,7 @@ package org.video.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
 public class VideoLibraryApplication {
 	public static void main(String[] args){
